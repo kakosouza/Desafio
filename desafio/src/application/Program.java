7ded6b87f890dfe63ddfcac8902863d2ed23adc3
Carlos Souza <kakosouza@gmail.com>
2023-04-04 15:51:46 -0300

    Inclusao das classes Fornecedor e Empresa

---
+++ desafio/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Fornecedor;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Fornecedor forn = new Fornecedor("999999999", null, null, null, null, null, null, null, null, null, null, null);
+		System.out.println(forn);
+	}
+
+}
